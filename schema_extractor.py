9e8929f44639240acdb323597764060ec592ec63
spydir <d03206642@gmail.com>
2023-03-17 20:58:18 +0000

    working from sift-dev-mac for the first time.

--- schema_extractor.py
+++ schema_extractor.py
@@ -2,28 +2,32 @@ import ijson
 import json
 import os
 
-filename = '/home/sansforensics/Desktop/working/cttout_MCCONTROLRMPC_20230311_15_30_40_clean.json'
+def extract_schema(filepath):
+    with open(filepath, 'rb') as file:
+        parser = ijson.items(file, 'cyberTriageAgentOutput.item.cyberTriageOutputSection.item')
 
-def extract_schema(obj, schema):
-    if isinstance(obj, dict):
-        for key, value in obj.items():
-            if isinstance(value, list):
-                if len(value) > 0 and isinstance(value[0], dict):
-                    schema[key] = [extract_schema(v, {}) for v in value]
+        schema = {}
+
+        for obj in parser:
+            if isinstance(obj, dict):
+                for key, value in obj.items():
+                    if isinstance(value, list):
+                        if len(value) > 0 and isinstance(value[0], dict):
+                            schema[key] = [extract_schema(v) for v in value]
+                        else:
+                            schema[key] = [type(v).__name__ for v in value]
+                    elif isinstance(value, dict):
+                        schema[key] = extract_schema(value)
+                    else:
+                        schema[key] = type(value).__name__
+            elif isinstance(obj, list):
+                if len(obj) > 0 and isinstance(obj[0], dict):
+                    schema = [extract_schema(elem) for elem in obj]
                 else:
-                    schema[key] = [type(v).__name__ for v in value]
-            elif isinstance(value, dict):
-                schema[key] = extract_schema(value, {})
+                    schema = [type(elem).__name__ for elem in obj]
             else:
-                schema[key] = type(value).__name__
+                schema = type(obj).__name__
         return schema
-    elif isinstance(obj, list):
-        if len(obj) > 0 and isinstance(obj[0], dict):
-            return [extract_schema(elem, {}) for elem in obj]
-        else:
-            return [type(elem).__name__ for elem in obj]
-    else:
-        return type(obj).__name__
 
 def sort_schema(schema):
     sorted_schema = {}
@@ -55,23 +59,20 @@ def remove_duplicates(schema):
 
     return cleaned_schema
 
-with open(filename, 'rb') as file:
-    parser = ijson.items(file, 'cyberTriageAgentOutput.item.cyberTriageOutputSection.item')
+if __name__ == "__main__":
+    filepath = "/path/to/json/file.json"
 
-    schema = {}
+    schema = extract_schema(filepath)
 
-    for obj in parser:
-        extract_schema(obj, schema)
+    if schema:
+        sorted_schema = sort_schema(schema)
+        cleaned_schema = remove_duplicates(sorted_schema)
 
-if schema:
-    sorted_schema = sort_schema(schema)
-    cleaned_schema = remove_duplicates(sorted_schema)
+        schema_filename = os.path.splitext(filepath)[0] + "_schema.json"
 
-    schema_filename = os.path.splitext(filename)[0] + "_schema.json"
+        with open(schema_filename, 'w', encoding='utf-8') as schema_file:
+            json.dump(cleaned_schema, schema_file, ensure_ascii=False, indent=2)
 
-    with open(schema_filename, 'w', encoding='utf-8') as schema_file:
-        json.dump(cleaned_schema, schema_file, ensure_ascii=False, indent=2)
-
-    print(f"Schema of JSON data is saved to {schema_filename}")
-else:
-    print("Error: JSON data not found in file.")
+        print(f"Schema of JSON data is saved to {schema_filename}")
+    else:
+        print("Error: JSON data not found in file.")
