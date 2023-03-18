33827f02a64e4e3d18643b8e6c82f95c1f1918e1
spydir <d03206642@gmail.com>
2023-03-18 06:37:14 +0000

    updated json_cleaner to use native commands on SIFT Workstation

--- breacherator.py
+++ breacherator.py
@@ -2,13 +2,13 @@ import argparse
 import os
 import json
 import json_cleaner
-import schema_extractor
+import schema_exporter
 
 # Define command line arguments
 parser = argparse.ArgumentParser(prog='breacherator', add_help=False)
 parser.add_argument('directory', nargs='?', default='.', help='Directory containing JSON files')
 parser.add_argument('-l', '--list', action='store_true', help='List JSON files in directory')
-parser.add_argument('-e', '--extract', action='store_true', help='Extract schema from JSON files in directory')
+parser.add_argument('-e', '--export', action='store_true', help='Export schema from JSON files in directory')
 parser.add_argument('-c', '--clean', action='store_true', help='Clean JSON files in directory')
 parser.add_argument('--help', action='help', help='Show this help message and exit')
 
@@ -22,12 +22,12 @@ if args.list:
         print(os.path.join(args.directory, f))
 
 # Extract schema from JSON files in directory
-elif args.extract:
+elif args.export:
     for filename in os.listdir(args.directory):
         if filename.endswith('_clean.json'):
             filepath = os.path.join(args.directory, filename)
             print(filepath)
-            schema = schema_extractor.extract_schema_from_file(filepath)
+            schema = schema_exporter.extract_schema_from_file(filepath)
             print(json.dumps(schema, indent=2))
 
 # Clean JSON files in directory
--- json_cleaner.py
+++ json_cleaner.py
@@ -1,49 +1,32 @@
 import os
 import re
-import gzip
+import subprocess
 
 # Regular expression pattern to match non-ASCII characters
 non_ascii_pattern = '[^\x00-\x7F\x80-\xFF\u001c]+'
 
 
 # Replace non-ASCII characters with spaces
-def clean_string(s):
-    cleaned = re.sub(non_ascii_pattern, ' ', s)
-
-    # Handle file separator character
-    cleaned = re.sub('\x1c', '', cleaned)
-
-    return ''.join(ch if ch.isprintable() or ch.isspace() else ' ' for ch in cleaned)
-
 
 def clean_json_file(filepath):
-    if filepath.endswith('.gz'):
+    if filepath.endswith('.json.gz'):
         # Create a new filename for the cleaned JSON file
-        cleaned_filename = os.path.splitext(filepath)[0] + "_clean.json"
+        dirname, basename = os.path.split(filepath)
+        filename, ext = os.path.splitext(basename)
+        filename, ext = os.path.splitext(filename)
+        tmp_filename = os.path.join(dirname, f"{filename}.tmp")
 
-        try:
-            # Open the input file as a gzip file
-            print(f"Reading {filepath}...")
-            with gzip.open(filepath, 'rb') as input_file:
-                # Open the output file as a regular file
-                with open(cleaned_filename, 'wb') as output_file:
-                    # Process the file in 1 MB chunks
-                    chunk_size = 1024 * 1024
-                    while True:
-                        chunk = input_file.read(chunk_size)
-                        if not chunk:
-                            break
-                        # Decode the chunk and clean it
-                        chunk_decoded = chunk.decode('utf-8', 'ignore')
-                        chunk_cleaned = clean_string(chunk_decoded)
-                        # Write the cleaned chunk to the output file
-                        chunk_encoded = chunk_cleaned.encode('utf-8')
-                        output_file.write(chunk_encoded)
+        # Call zgrep command to remove "fileContent" key:value pair
+        print(f"Removing 'fileContent' key:value pair from {filepath}...")
+        with open(tmp_filename, 'w') as f:
+            subprocess.run(['zgrep', '-v', '"fileContent" :', filepath], stdout=f)
 
-            print(f"Cleaning of JSON file is complete. Results are saved to {cleaned_filename}")
+        # Call tr command to remove bad characters
+        cleaned_filename = os.path.join(dirname, f"{filename}_clean.json")
+        print(f"Cleaning {cleaned_filename}...")
+        subprocess.run(['chmod', '+x', './json_cleaner.sh'])
+        subprocess.run(['./json_cleaner.sh', tmp_filename, cleaned_filename])
 
-        except EOFError:
-            print(f"Error: {filepath} is a corrupt .json.gz file.")
     else:
         print(f"Error: {filepath} is not a .json.gz file.")
 
